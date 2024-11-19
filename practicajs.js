be43d3e620932d1d53f03e9768da74c9c53e8e1a
jicespedes <jicespedes1976@gmail.com>
2024-11-19 07:08:09 -0600

    18 de nov

---
+++ practicajs.js
@@ -0,0 +1,7 @@
+let num1 = 90;
+let num2 = 10;
+if (num1 > num2) {
+    console.log('num1 es mayor', num1)
+} else {
+    console.log('num2 es mayor')
+}
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('¡Hola, mundo desde el backend!');
+    res.end('¡Bienvenido al backend!');
 });
 
 server.listen(3000, () => {
